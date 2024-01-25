52f4ba2c120a2fd42de28cd26226368edc3df0bb
Dmitry Tsurkan <dlink13@icloud.com>
2024-01-25 17:16:20 +0200

    Remove default value for the closure in production (moved to test scope)

--- EssentialFeed/EssentialFeed/FeedAPI/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/FeedAPI/RemoteFeedLoader.swift
@@ -14,7 +14,7 @@ public final class RemoteFeedLoader {
         self.url = url
     }
     
-    public func load(completion: @escaping (RemoteFeedLoader.Error) -> Void = { _ in }) {
+    public func load(completion: @escaping (RemoteFeedLoader.Error) -> Void) {
         client.get(from: url) { error in completion(.connectivity) }
     }
 }
