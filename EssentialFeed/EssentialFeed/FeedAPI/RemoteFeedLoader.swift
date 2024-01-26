be9b3be06debc9c31ec01eb2fa8b8193afc4b03e
Dmitry Tsurkan <dlink13@icloud.com>
2024-01-26 17:25:22 +0200

    Replace magic number with a DSL

--- EssentialFeed/EssentialFeed/FeedAPI/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/FeedAPI/RemoteFeedLoader.swift
@@ -50,8 +50,10 @@ private final class FeedItemsMapper {
         }
     }
     
+    static var OK_200: Int { 200 }
+    
     static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
-        guard response.statusCode == 200 else {
+        guard response.statusCode == OK_200 else {
             throw RemoteFeedLoader.Error.invalidData
         }
         let root = try JSONDecoder().decode(Root.self, from: data)
