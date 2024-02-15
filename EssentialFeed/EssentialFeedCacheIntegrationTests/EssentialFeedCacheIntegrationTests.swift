716488c3d89cd01c784316052e0ff477ce7ff1c4
Dmitry Tsurkan <linkmoody@gmail.com>
2024-02-15 21:49:10 +0200

    LocalFeedLoader in integration with the CoreDataFeedStore delivers no items on empty cache

--- EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -1,7 +1,41 @@
 import XCTest
+import EssentialFeed
 
 final class EssentialFeedCacheIntegrationTests: XCTestCase {
     
+    func test_load_deliversEmptyOnEmptyCache() {
+        let sut = makeSUT()
+        
+        let exp = expectation(description: "Wait for completion")
+        
+        sut.load { result in
+            switch result {
+            case let .success(feed):
+                XCTAssertEqual(feed, [], "Expected empty feed")
+            case let .failure(error):
+                XCTFail("Expected to load feed successfully, got \(error) instead")
+            }
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
     
+    // MARK: - Helpers
     
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> LocalFeedLoader {
+        let storeBundle = Bundle(for: CoreDataFeedStore.self)
+        let storeURL = storeURL()
+        let store = try! CoreDataFeedStore(storeURL: storeURL, bundle: storeBundle)
+        let sut = LocalFeedLoader(store: store, currentDate: Date.init)
+        return sut
+    }
+    
+    private func storeURL() -> URL {
+        cachesDirectory().appendingPathExtension("\(type(of: self)).store")
+    }
+    
+    private func cachesDirectory() -> URL {
+        FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+    }
 }
