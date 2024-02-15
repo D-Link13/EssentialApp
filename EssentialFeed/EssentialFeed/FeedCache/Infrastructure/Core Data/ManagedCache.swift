4103e66183b0fb8801f03d4d09bbdfdf1a81fb1e
Dmitry Tsurkan <linkmoody@gmail.com>
2024-02-15 14:09:26 +0200

    Extract CoreData managed model classes into separate files

---
+++ EssentialFeed/EssentialFeed/FeedCache/Infrastructure/Core Data/ManagedCache.swift
@@ -0,0 +1,26 @@
+import CoreData
+
+@objc(ManagedCache)
+class ManagedCache: NSManagedObject {
+    
+    @NSManaged var timestamp: Date
+    @NSManaged var feed: NSOrderedSet
+    
+    var localFeed: [LocalFeedImage] {
+        feed
+            .compactMap { $0 as? ManagedFeedImage }
+            .map { LocalFeedImage(id: $0.id, description: $0.imageDescription, location: $0.location, url: $0.url) }
+    }
+    
+    static func find(in context: NSManagedObjectContext) throws -> ManagedCache? {
+        let request = NSFetchRequest<ManagedCache>(entityName: ManagedCache.entity().name!)
+        request.returnsObjectsAsFaults = false
+        return try context.fetch(request).first
+    }
+    
+    static func newUniqueInstance(in context: NSManagedObjectContext) throws -> ManagedCache {
+        try find(in: context).map(context.delete)
+        return ManagedCache(context: context)
+    }
+    
+}
---
+++ EssentialFeed/EssentialFeed/FeedCache/Infrastructure/Core Data/ManagedFeedImage.swift
@@ -0,0 +1,25 @@
+import CoreData
+
+@objc(ManagedFeedImage)
+class ManagedFeedImage: NSManagedObject {
+    
+    @NSManaged var id: UUID
+    @NSManaged var imageDescription: String?
+    @NSManaged var location: String?
+    @NSManaged var url: URL
+    @NSManaged var cache: ManagedCache
+    
+    static func images(from feed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
+        let managedFeed = feed.map { local in
+            let managedImage = ManagedFeedImage(context: context)
+            managedImage.id = local.id
+            managedImage.imageDescription = local.description
+            managedImage.location = local.location
+            managedImage.url = local.url
+            return managedImage
+        }
+        return NSOrderedSet(array: managedFeed)
+    }
+    
+}
+
