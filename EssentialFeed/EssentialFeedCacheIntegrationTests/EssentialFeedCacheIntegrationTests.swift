60edb870cd96af65876ca10b066beee26534da45
Dmitry Tsurkan <linkmoody@gmail.com>
2024-02-15 21:50:26 +0200

    Include memory leak tracking helper in the cache integration tests target

--- EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -28,6 +28,8 @@ final class EssentialFeedCacheIntegrationTests: XCTestCase {
         let storeURL = storeURL()
         let store = try! CoreDataFeedStore(storeURL: storeURL, bundle: storeBundle)
         let sut = LocalFeedLoader(store: store, currentDate: Date.init)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
         return sut
     }
     
