2ccb444fa448395a1bd1113eb73447eac05825ec
Dmitry Tsurkan <linkmoody@gmail.com>
2024-03-01 15:43:30 +0200

    Move FeedLoaderPresentationAdapter to separate file

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedLoaderPresentationAdapter.swift
@@ -0,0 +1,31 @@
+import EssentialFeed
+
+final class FeedLoaderPresentationAdapter {
+    
+    private let feedLoader: FeedLoader
+    var presenter: FeedViewPresenter?
+    
+    init(feedLoader: FeedLoader) {
+        self.feedLoader = feedLoader
+    }
+    
+    func loadFeed() {
+        presenter?.didStartLoadingFeed()
+        
+        feedLoader.load { [weak self] result in
+            switch result {
+            case let .success(feed):
+                self?.presenter?.didFinishLoadingFeed(with: feed)
+            case let .failure(error):
+                self?.presenter?.didFinishLoadingFeed(with: error)
+            }
+        }
+    }
+}
+
+extension FeedLoaderPresentationAdapter: FeedViewControllerDelegate {
+    
+    func didRequestFeedRefresh() {
+        loadFeed()
+    }
+}
--- EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -47,33 +47,3 @@ final class FeedViewAdapter: FeedView {
         }
     }
 }
-
-final class FeedLoaderPresentationAdapter {
-    
-    private let feedLoader: FeedLoader
-    var presenter: FeedViewPresenter?
-    
-    init(feedLoader: FeedLoader) {
-        self.feedLoader = feedLoader
-    }
-    
-    func loadFeed() {
-        presenter?.didStartLoadingFeed()
-        
-        feedLoader.load { [weak self] result in
-            switch result {
-            case let .success(feed):
-                self?.presenter?.didFinishLoadingFeed(with: feed)
-            case let .failure(error):
-                self?.presenter?.didFinishLoadingFeed(with: error)
-            }
-        }
-    }
-}
-
-extension FeedLoaderPresentationAdapter: FeedViewControllerDelegate {
-    
-    func didRequestFeedRefresh() {
-        loadFeed()
-    }
-}
