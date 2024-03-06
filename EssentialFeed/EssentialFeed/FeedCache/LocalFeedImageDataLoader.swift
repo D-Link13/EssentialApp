bc139f3283bef2eab31eaad089e131e3dc083dc7
Dmitry Tsurkan <linkmoody@gmail.com>
2024-03-06 17:40:32 +0200

    Move LocalFeedImageDataLoader and FeedImageDataStore to production

---
+++ EssentialFeed/EssentialFeed/FeedCache/LocalFeedImageDataLoader.swift
@@ -0,0 +1,50 @@
+import Foundation
+
+public final class LocalFeedImageDataLoader {
+    
+    private let store: FeedImageDataStore
+    
+    public init(store: FeedImageDataStore) {
+        self.store = store
+    }
+    
+    public enum Error: Swift.Error {
+        case failed
+        case notFound
+    }
+    
+    private final class Task: FeedImageDataLoaderTask {
+        private var completion: ((FeedImageDataLoader.Result) -> Void)?
+        
+        init(_ completion: @escaping (FeedImageDataLoader.Result) -> Void) {
+            self.completion = completion
+        }
+        
+        func complete(with result: FeedImageDataLoader.Result) {
+            completion?(result)
+        }
+        
+        func cancel() {
+            preventFutureCompletions()
+        }
+        
+        private func preventFutureCompletions() {
+            completion = nil
+        }
+    }
+    
+    public func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+        let task = Task(completion)
+        store.retrieve(dataForURL: url) { [weak self] result in
+            guard self != nil else { return }
+            
+            task.complete(with: result
+                .mapError { _ in Error.failed }
+                .flatMap { data in
+                    data.map { .success($0) } ?? .failure(Error.notFound)
+                }
+            )
+        }
+        return task
+    }
+}
