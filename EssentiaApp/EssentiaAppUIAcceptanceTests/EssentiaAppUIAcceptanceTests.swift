6930293d8e3705d01856cf59346be5ac9ef04353
Dmitry Tsurkan <linkmoody@gmail.com>
2024-03-25 14:26:51 +0200

    Displays remote feed on launch when customer has connectivity

--- EssentiaApp/EssentiaAppUIAcceptanceTests/EssentiaAppUIAcceptanceTests.swift
+++ EssentiaApp/EssentiaAppUIAcceptanceTests/EssentiaAppUIAcceptanceTests.swift
@@ -1,6 +1,14 @@
 import XCTest
 
 final class EssentiaAppUIAcceptanceTests: XCTestCase {
-
+    
+    func test_onLaunch_displaysRemoteFeedWhenCustomerHasConnectivity() {
+        let app = XCUIApplication()
+        
+        app.launch()
+        
+        XCTAssertEqual(app.cells.count, 22)
+        XCTAssertEqual(app.cells.firstMatch.images.count, 1)
+    }
     
 }
