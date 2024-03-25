6be135b6e5370029b952a5e44a3c4015ffbe218c
Dmitry Tsurkan <linkmoody@gmail.com>
2024-03-25 16:05:24 +0200

    Reset cache in every test to make sure tests run in a clean state and don't influence the result of other tests

--- EssentiaApp/EssentiaAppUIAcceptanceTests/EssentiaAppUIAcceptanceTests.swift
+++ EssentiaApp/EssentiaAppUIAcceptanceTests/EssentiaAppUIAcceptanceTests.swift
@@ -4,7 +4,7 @@ final class EssentiaAppUIAcceptanceTests: XCTestCase {
     
     func test_onLaunch_displaysRemoteFeedWhenCustomerHasConnectivity() {
         let app = XCUIApplication()
-        
+        app.launchArguments = ["-reset"]
         app.launch()
         
         let feedCells = app.cells.matching(identifier: "feed-image-cell")
@@ -16,6 +16,7 @@ final class EssentiaAppUIAcceptanceTests: XCTestCase {
     
     func test_onLaunch_displaysCachedRemoteFeedWhenCustomerHasNoConnectivity() {
         let onlineApp = XCUIApplication()
+        onlineApp.launchArguments = ["-reset"]
         onlineApp.launch()
         
         let offlineApp = XCUIApplication()
