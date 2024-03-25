8e0273e6d4f6023dab953a90e270636fc6b6b029
Dmitry Tsurkan <linkmoody@gmail.com>
2024-03-25 13:34:03 +0200

    Loader Cache Decorators (#5)
    * FeedLoaderCacheDecorator.load delivers decoratee loader result (either success or failure)
    
    * Extract FeedLoaderStub into a shared scope to remove duplication
    
    * Extract uniqueFeed factory helper into a shared scope to remove duplication
    
    * Extract FeedLoader test helpers into a shared protocol extension to remove duplication
    
    * Extract system under test (SUT) creation into a factory method
    
    * FeedLoaderCacheDecorator.load caches loaded feed on loader success
    
    * FeedLoaderCacheDecorator.load does not cache feed on loader failure
    
    * Replace if-try-statement with map
    
    * Move FeedCache to the Feed Feature production group
    
    * Make the LocalFeedLoader implement the FeedCache protocol (so it can be composed)
    
    * Move FeedLoaderCacheDecorator to production
    
    * Create saveIgnoringResult method to clarify intent
    
    * FeedImageDataLoaderCacheDecorator.loadImageData delivers decoratee result (either success or failure)
    
    * Extract FeedImageDataLoaderSpy into a shared scope to remove duplication
    
    * Extract FeedImageDataLoader test helpers into a shared protocol extension to remove duplication
    
    * FeedImageDataLoaderCacheDecorator.loadImageData caches loaded data on loader success
    
    * FeedImageDataLoaderCacheDecorator.loadImageData does not cache image data on loader failure
    
    * Move FeedImageDataCache to the Feed Feature production group
    
    * Make the LocalFeedImageDataLoader implement the `FeedImageDataCache` protocol (so it can be composed)
    
    * Move FeedImageDataLoaderCacheDecorator to production
    
    * Create saveIgnoringResult method to clarify intent

---
+++ EssentiaApp/EssentiaApp/FeedLoaderCacheDecorator.swift
@@ -0,0 +1,28 @@
+import Foundation
+import EssentialFeed
+
+public final class FeedLoaderCacheDecorator: FeedLoader {
+    private let decoratee: FeedLoader
+    private let cache: FeedCache
+
+    public init(decoratee: FeedLoader, cache: FeedCache) {
+        self.decoratee = decoratee
+        self.cache = cache
+    }
+
+    public func load(completion: @escaping (FeedLoader.Result) -> Void) {
+        decoratee.load { [weak self] result in
+            completion(result.map { feed in
+                self?.cache.saveIgnoringResult(feed)
+                return feed
+            })
+        }
+    }
+}
+
+private extension FeedCache {
+    
+    func saveIgnoringResult(_ feed: [FeedImage]) {
+        save(feed, completion: { _ in })
+    }
+}
