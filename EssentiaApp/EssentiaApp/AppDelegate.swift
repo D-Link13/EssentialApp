a4b22c37263451533149af99e948d6ffc3dcff60
Dmitry Tsurkan <linkmoody@gmail.com>
2024-03-26 14:07:02 +0200

    Remove unused debugging code that was previously used during UI tests

--- EssentiaApp/EssentiaApp/AppDelegate.swift
+++ EssentiaApp/EssentiaApp/AppDelegate.swift
@@ -8,21 +8,5 @@
 import UIKit
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        return true
-    }
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        let configuration = UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-        
-        #if DEBUG
-        configuration.delegateClass = DebuggingSceneDelegate.self
-        #endif
-        
-        return configuration
-    }
-
-}
+class AppDelegate: UIResponder, UIApplicationDelegate {}
 
--- EssentiaApp/EssentiaApp/SceneDelegate.swift
+++ EssentiaApp/EssentiaApp/SceneDelegate.swift
@@ -13,17 +13,15 @@ import CoreData
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-    
-    let localStoreURL = NSPersistentContainer
-        .defaultDirectoryURL()
-        .appendingPathComponent("feed-store.sqlite")
 
     private lazy var httpClient: HTTPClient = {
         URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
     }()
     
     private lazy var store: FeedStore & FeedImageDataStore = {
-        try! CoreDataFeedStore(storeURL: localStoreURL)
+        try! CoreDataFeedStore(storeURL: NSPersistentContainer
+            .defaultDirectoryURL()
+            .appendingPathComponent("feed-store.sqlite"))
     }()
     
     convenience init(httpClient: HTTPClient, store: FeedStore & FeedImageDataStore) {
@@ -41,9 +39,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func configureWindow() {
         let remoteURL = URL(string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5db4155a4fbade21d17ecd28/1572083034355/essential_app_feed.json")!
         
-        let remoteClient = makeRemoteClient()
-        let remoteFeedLoader = RemoteFeedLoader(client: remoteClient, url: remoteURL)
-        let remoteImageLoader = RemoteFeedImageDataLoader(client: remoteClient)
+        let remoteFeedLoader = RemoteFeedLoader(client: httpClient, url: remoteURL)
+        let remoteImageLoader = RemoteFeedImageDataLoader(client: httpClient)
         
         let localFeedLoader = LocalFeedLoader(store: store, currentDate: Date.init)
         let localFeedImageLoader = LocalFeedImageDataLoader(store: store)
@@ -60,10 +57,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                     fallback: FeedImageDataLoaderCacheDecorator(
                         decoratee: remoteImageLoader,
                         cache: localFeedImageLoader))))
-    }
-    
-    func makeRemoteClient() -> HTTPClient {
-        httpClient
     }
 
 }
