cf1faf4a55b13197f5f6a1c2460af3dbc1f9a832
Dmitry Tsurkan <linkmoody@gmail.com>
2024-03-26 13:49:43 +0200

    Displays empty feed on launch when customer has no connectivity and no cache

--- EssentiaApp/EssentiaAppTests/FeedAcceptanceTests.swift
+++ EssentiaApp/EssentiaAppTests/FeedAcceptanceTests.swift
@@ -27,7 +27,9 @@ final class FeedAcceptanceTests: XCTestCase {
     }
     
     func test_onLaunch_displaysEmptyFeedWhenCustomerHasNoConnectivityAndNoCache() {
+        let feed = launch(httpClient: .offline, store: .empty)
         
+        XCTAssertEqual(feed.numberOfRenderedFeedImages(), 0)
     }
     
     // MARK: - Helpers
