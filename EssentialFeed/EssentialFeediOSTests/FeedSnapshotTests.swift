e18d738e0e62a7660e184ca24351eeb83126c9cf
Dmitry Tsurkan <linkmoody@gmail.com>
2024-03-26 18:54:54 +0200

    Record empty feed snapshot

---
+++ EssentialFeed/EssentialFeediOSTests/FeedSnapshotTests.swift
@@ -0,0 +1,60 @@
+import XCTest
+import EssentialFeediOS
+
+final class FeedSnapshotTests: XCTestCase {
+    
+    func test_emptyFeed() {
+        let sut = makeSUT()
+        
+        sut.display(emptyFeed())
+        
+        record(snapshot: sut.snapshot(), name: "EMPTY_FEED")
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT() -> FeedViewController {
+        let bundle = Bundle(for: FeedViewController.self)
+        let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
+        let controller = storyboard.instantiateInitialViewController() as! FeedViewController
+        controller.loadViewIfNeeded()
+        return controller
+    }
+    
+    private func emptyFeed() -> [FeedImageCellController] {
+        []
+    }
+    
+    private func record(snapshot: UIImage, name: String, file: StaticString = #filePath, line: UInt = #line) {
+        
+        guard let snapshotData = snapshot.pngData() else {
+            return XCTFail("Failed to generate PNG data representation from snapshot", file: file, line: line)
+        }
+        
+        let snapshotURL = URL(fileURLWithPath: String(describing: file))
+            .deletingLastPathComponent()
+            .appendingPathComponent("snapshots")
+            .appendingPathComponent("\(name).png")
+        
+        do {
+            try FileManager.default.createDirectory(
+                at: snapshotURL.deletingLastPathComponent(),
+                withIntermediateDirectories: true)
+            
+            try snapshotData.write(to: snapshotURL)
+        } catch {
+            XCTFail("Failed to record snapshot with error: \(error)", file: file, line: line)
+        }
+    }
+    
+}
+
+private extension UIViewController {
+    
+    func snapshot() -> UIImage {
+        let renderer = UIGraphicsImageRenderer(bounds: view.bounds)
+        return renderer.image { action in
+            view.layer.render(in: action.cgContext)
+        }
+    }
+}
