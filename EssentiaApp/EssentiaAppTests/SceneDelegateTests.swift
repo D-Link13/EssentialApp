158d315c6a0492a1ef8423eed950e9818ad5d7ef
Dmitry Tsurkan <linkmoody@gmail.com>
2024-04-16 20:36:04 +0300

    Remove unnecessary Main storyboard - the initial set up happens in code

--- EssentiaApp/EssentiaAppTests/SceneDelegateTests.swift
+++ EssentiaApp/EssentiaAppTests/SceneDelegateTests.swift
@@ -4,6 +4,18 @@ import EssentialFeediOS
 
 final class SceneDelegateTests: XCTestCase {
     
+    func test_configureWindow_setsWindowAsKeyAndVisible() {
+        let window = UIWindow()
+        window.windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
+        let sut = SceneDelegate()
+        sut.window = window
+        
+        sut.configureWindow()
+        
+        XCTAssertTrue(window.isKeyWindow, "Expected window to be the key window")
+        XCTAssertFalse(window.isHidden, "Expected window to be visible")
+    }
+    
     func test_sceneWillConnectToSession_configuresRootViewController() {
         let sut = SceneDelegate()
         sut.window = UIWindow()
