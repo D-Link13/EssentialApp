ad419138e95b7bac5f52862c62a4c4f1ab74dbb8
Dmitry Tsurkan <linkmoody@gmail.com>
2024-04-29 15:56:45 +0300

    Duplicate RemoteFeedLoader as RemoteImageCommentsLoader

---
+++ EssentialFeed/EssentialFeed/FeedAPI/RemoteImageCommentsLoader.swift
@@ -0,0 +1,46 @@
+import Foundation
+
+public final class RemoteImageCommentsLoader: FeedLoader {
+    private let client: HTTPClient
+    private let url: URL
+    
+    public enum Error: Swift.Error {
+        case connectivity
+        case invalidData
+    }
+    
+    public typealias Result = FeedLoader.Result
+    
+    public init(client: HTTPClient, url: URL) {
+        self.client = client
+        self.url = url
+    }
+    
+    public func load(completion: @escaping (Result) -> Void) {
+        client.get(from: url) { [weak self] result in
+            guard self != nil else { return }
+            switch result {
+            case let .success((data, response)):
+                completion(RemoteImageCommentsLoader.map(data: data, response: response))
+            case .failure:
+                completion(.failure(Error.connectivity))
+            }
+        }
+    }
+    
+    private static func map(data: Data, response: HTTPURLResponse) -> RemoteImageCommentsLoader.Result {
+        do {
+            let remoteItems = try ImageCommentsMapper.map(data, response)
+            return .success(remoteItems.toItems())
+        } catch {
+            return .failure(error)
+        }
+    }
+}
+
+private extension Array where Element == RemoteFeedItem {
+    
+    func toItems() -> [FeedImage] {
+        map { FeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.image) }
+    }
+}
