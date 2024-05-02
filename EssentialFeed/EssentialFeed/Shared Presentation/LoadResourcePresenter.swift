f1012394eab9ba54e444f40a01ea1bc14838d3c9
Dmitry Tsurkan <linkmoody@gmail.com>
2024-05-02 22:40:59 +0300

    Remote title from generic presenter since it's specific to each presenter

--- EssentialFeed/EssentialFeed/Shared Presentation/LoadResourcePresenter.swift
+++ EssentialFeed/EssentialFeed/Shared Presentation/LoadResourcePresenter.swift
@@ -12,13 +12,6 @@ public final class LoadResourcePresenter {
         self.errorView = errorView
     }
     
-    public static var title: String {
-        return NSLocalizedString("FEED_VIEW_TITLE",
-                                 tableName: "Feed",
-                                 bundle: Bundle(for: FeedPresenter.self),
-                                 comment: "Title for the feed view")
-    }
-    
     private var feedLoadError: String {
         return NSLocalizedString("FEED_VIEW_CONNECTION_ERROR",
                                  tableName: "Feed",
