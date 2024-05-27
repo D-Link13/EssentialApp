b333e52fb39542d50237cc6c671de0573feb841d
Dmitry Tsurkan <linkmoody@gmail.com>
2024-05-27 22:52:55 +0300

    Proved request is canceled on comments view deinit

--- EssentiaApp/EssentiaAppTests/ImageCommentsUIIntegrationTests.swift
+++ EssentiaApp/EssentiaAppTests/ImageCommentsUIIntegrationTests.swift
@@ -141,6 +141,27 @@ final class ImageCommentsUIIntegrationTests: XCTestCase {
         XCTAssertEqual(sut.errorMessage, nil)
     }
     
+    func test_deinit_cancelsRunningRequests() {
+        var cancelCallCount = 0
+        var sut: ListViewController?
+        autoreleasepool {
+            sut = CommentsUIComposer.commentsComposedWith(
+                commentsLoader: {
+                    PassthroughSubject<[ImageComment], Error>()
+                        .handleEvents(receiveCancel: {
+                            cancelCallCount += 1
+                        })
+                        .eraseToAnyPublisher()
+                })
+            
+            sut?.simulateAppearance()
+        }
+        XCTAssertEqual(cancelCallCount, 0)
+        
+        sut = nil
+        XCTAssertEqual(cancelCallCount, 1)
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: ListViewController, loader: LoaderSpy) {
